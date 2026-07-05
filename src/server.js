91314ba19e9f53d5e237e3617084c91a50d79917
VolodymyrB <volodymyrbedryk@gmail.com>
2026-07-05 21:24:56 +0200

    changed

--- src/server.js
+++ src/server.js
@@ -33,8 +33,11 @@ app.get('/notes', (req, res) => {
 });
 
 app.get('/notes/:noteId', (req, res) => {
-  const { noteId } = req.params.noteId;
-  res.status(200).json({ id: noteId });
+  const { noteId } = req.params;
+
+  res.status(200).json({
+    message: `Retrieved note with ID: ${noteId}`,
+  });
 });
 app.get('/test-error', (req, res) => {
   throw new Error('Simulated server error');
