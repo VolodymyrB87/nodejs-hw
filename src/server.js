24fc11a37f00050b257cf2b06970c0632f5933c3
VolodymyrB <volodymyrbedryk@gmail.com>
2026-08-02 14:32:20 +0200

    added routs

--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
 import cookieParser from 'cookie-parser';
 import userRoutes from './routes/userRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
@@ -23,7 +24,7 @@ app.use(cookieParser());
 app.use(logger);
 app.use(helmet());
 app.use(express.json());
-
+app.use(authRoutes);
 app.use(notesRoutes);
 app.use(userRoutes);
 app.use(notFoundHandler);
