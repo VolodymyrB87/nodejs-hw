7bde9efe63de845da1c350731905fec2444bc1ea
VolodymyrB <volodymyrbedryk@gmail.com>
2026-08-02 13:44:27 +0200

    added some code

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
 import cookieParser from 'cookie-parser';
+import userRoutes from './routes/userRoutes.js';
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
@@ -24,7 +25,7 @@ app.use(helmet());
 app.use(express.json());
 
 app.use(notesRoutes);
-
+app.use(userRoutes);
 app.use(notFoundHandler);
 app.use(errors());
 app.use(errorHandler);
